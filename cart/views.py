5242a746a519f8fb43299e03d4a3991aad8ac6ab
DuyManh1905 <93384645+DuyManh1905@users.noreply.github.com>
2024-01-23 23:58:35 +0700

    final version 1

--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from . import views
+from django.template import RequestContext
 from cart import cart
 
 
-# Create your views here.
 def show_cart(request, template_name="cart/cart.html"):
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+
+        if postdata.get('submit') == 'Remove':
+            cart.remove_from_cart(request)
+
+        if postdata.get('submit') == 'Update':
+            cart.update_cart(request)
+
     cart_items = cart.get_cart_items(request)
-    print('cart_items', cart_items)
-    page_title = "Shopping Cart"
+    page_title = 'Shopping Cart'
     cart_subtotal = cart.cart_subtotal(request)
-    print('subtotal', cart_subtotal)
-    cart_item_count = len(cart_items)
-    return render(request, template_name,  locals(),)
+
+    return render(request, template_name, {'cart_items': cart_items, 'page_title': page_title,
+                                            'cart_subtotal': cart_subtotal})
